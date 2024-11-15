3c8b4cd3092a303da892296e5e160c58841e277a
ryo_numoto <info@craftteck.com>
2024-11-15 18:42:36 +0900

    初期セットアップ

---
+++ laravel/.php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.1.0|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'ordered_imports' => true,
+        'no_unused_imports' => true,
+        'single_quote' => true,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->exclude([
+            'vendor'
+        ])
+        ->in(__DIR__)
+    )
+;
